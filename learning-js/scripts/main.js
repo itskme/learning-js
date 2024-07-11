4f1e494b13d0236325d02037a7ff4eff6cf362d1
itskme <160423464+itskme@users.noreply.github.com>
2024-07-11 04:22:30 +0000

    my first real knowledge of js
    practise makes perfect.

---
+++ learning-js/scripts/main.js
@@ -0,0 +1,27 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+
+let myVariable;
+myVariable = "Bob";
+
+let arrayPractise
+arrayPractise = [1,lol,2,lol2,3]
+
+arrayPractise[1]
+
+/*comment practise
+
+this is a comment. */
+
+let addition
+addition = arrayPractise[1]+myVariable
+
+let iceCream = "chocolate";
+if (iceCream === "chocolate") {
+  alert("Yay, I love chocolate ice cream!");
+} else {
+  alert("Awwww, but chocolate is my favorite…");
+}
+
+/*function practise */
+
